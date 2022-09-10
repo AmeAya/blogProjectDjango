0c28e91a0f2da2aa804c7533fbe741a3f7e7b5a5
AmeAmeAya <ameameameaya@gmail.com>
2022-09-10 20:48:10 +0600

    DetailView

--- blogApp/models.py
+++ blogApp/models.py
@@ -6,6 +6,15 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',
                                on_delete=models.CASCADE)
     body = models.TextField()
+    language = models.CharField(max_length=2,
+                                default='EN',
+                                choices=[
+                                    ('EN', 'English'),
+                                    ('RU', 'Russian'),
+                                    ('KZ', 'Kazakh'),
+                                    ('FR', 'French'),
+                                    ('GR', 'Germanic'),
+                                ])
 
     def __str__(self):
         return self.title
